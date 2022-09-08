f2701a5a01b38f9712260c882077c6ac851e8cd4
Bytangle <dev.bytangle@gmail.com>
2022-09-08 14:50:05 +0100

    Update DividentContract to use the withdrawal pattern

--- contracts/DividentContract.sol
+++ contracts/DividentContract.sol
@@ -2,29 +2,54 @@
 
 pragma solidity ^0.8.16;
 
+import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
 /**
  * @title Divident Contract
  * @author Edicha Joshua <mredichaj@gmail.com>
  */
-contract DividentContract {
+contract DividentContract is Ownable {
 
-    address[] private _investors;
+    mapping(address => uint256 ) private _balances;
 
+    /// @dev zero address error
+    /// @param _msg friendly message :)
     error ZeroAddress(string _msg);
 
+    /// @dev investor registration event
+    /// @param _addr address of the investor registered
     event InvestorRegistration(address indexed _addr);
+    
+    /// @dev divident claim event
+    /// @param _addr address of the investor
+    event DividentClaimed(address indexed _addr);
 
     /**
      * @notice add new investor
      * @dev registering zero address reverts the transaction
      * @param _addr address of the investor
      */
-    function registerInvestor(address _addr) public {
+    function registerInvestor(address _addr) public payable onlyOwner {
         if(_addr == address(0)) revert ZeroAddress({_msg: "Zero address provided"});
 
-        _investors.push(_addr); // register investor
+        _balances[_addr] = msg.value; // register investor with initial investment
 
         emit InvestorRegistration({_addr: _addr}); // emit event
     }
+
+    /// @notice claim divident
+    function claimDivident() public {
+        require(_balances[msg.sender] > 0, "You dont have any investment");
+        
+        uint256 amt = _balances[msg.sender];
+
+        delete _balances[msg.sender]; // reset invested amount
+
+        payable(msg.sender).transfer(amt); // pay investor
+
+        emit DividentClaimed(msg.sender); // emit event
+    }
+
+
 
 }
