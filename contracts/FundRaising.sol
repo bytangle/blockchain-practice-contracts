50509ab22e3a6c38a4331be9afdf8798087cb997
Bytangle <dev.bytangle@gmail.com>
2022-09-09 15:00:37 +0100

    Complete FundRaising.sol implementation

--- contracts/FundRaising.sol
+++ contracts/FundRaising.sol
@@ -10,7 +10,13 @@ contract FundRaising {
     uint256 private _targetAmount; // target amount
 
     /// @dev donations => address and the amount donated
-    mapping(address => uint256) private _donations;
+    DonationInfo[] private _donations;
+
+    /// @dev donation details struct
+    struct DonationInfo {
+        address donator;
+        uint256 amount;
+    }
 
     /// @notice state definition
     enum Phase {
@@ -26,8 +32,19 @@ contract FundRaising {
     /// @dev emit when fund raising ends
     event FundRaisingEnded(uint256 _time, uint256 _totalAmountRaised);
 
+    /// @dev emit when a donator donates
+    /// @param _donator address of the person who donated
+    /// @param _amount amount donated
+    event Donation(address _donator, uint256 _amount);
+
     /// @dev used with revert when throwing as a result of unauthorized attempt to call a function
     error Unauthorized();
+
+    /// @dev fund raising hasn't started
+    error FundRaisingHasNotStarted();
+
+    /// @dev used with revert when fund raising is running
+    error FundRaisingIsRunning();
 
     /// @dev guard against unathorized access [only admin can call functions with this modifier]
     modifier onlyAdmin {
@@ -35,11 +52,68 @@ contract FundRaising {
         _;
     }
 
+    /// @dev ensure fund raising has started
+    modifier ensureFundRaisingStarted {
+        if(status == Phase.ENDED) revert FundRaisingHasNotStarted(); // ensure fund raising has started
+        _;
+    }
+
+    /// @dev ensure fund raising hasn't started
+    modifier ensureFundRaisingEnded {
+        if(status == Phase.STARTED) revert FundRaisingIsRunning(); // ensure fund raising has not started
+        _;    
+    }
+
     /**
      * @notice start fund raising
      * @param _target amount to achieve
      */
-    function beginFundRaising(uint256 _target) public onlyAdmin {
-        
+    function beginFundRaising(uint256 _target) public onlyAdmin ensureFundRaisingEnded {
+        require(_targetAmount == 0); // target amount must be zero
+
+        _targetAmount = _target; // set target
+
+        status = Phase.STARTED; // update status
+
+        emit FundRaisingStarted(block.timestamp); // emit event
+    }
+
+    /**
+     * @notice end fund raising
+     */
+    function endFundRaising() public onlyAdmin ensureFundRaisingStarted {
+
+        uint256 amt = _amount;
+
+        delete _targetAmount; // reset target amount to zero
+        delete _amount; // reset amount
+
+        status = Phase.ENDED; // update status
+
+        payable(_admin).transfer(amt); // transfer funds to the admin
+
+        emit FundRaisingEnded(block.timestamp, amt); // emit event
+    }
+
+    /// fallback function that'll be called when ether is sent directly to this contract
+    receive() external payable {
+        donate();
+    }
+
+    /// @notice donate to this contract
+    function donate() public payable ensureFundRaisingStarted {
+        require(msg.value > 0); // cannot donate 0 amount
+
+        _amount += msg.value; // update amount
+
+        _donations.push(DonationInfo(msg.sender, msg.value)); // save donation detail
+
+        emit Donation(msg.sender, msg.value); // emit event
+    }
+
+    /// @dev retrieve donations info
+    /// @return array of donation details {address, amount}
+    function getDonationDetails() public view returns (DonationInfo[] memory) {
+        return _donations;
     }
 }
